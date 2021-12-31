3a6063a1470cf405346a2c3699ca398a874ab4bb
Aman-Godara <amangodara891@gmail.com>
2022-01-01 01:14:11 +0530

    Integrate prettier

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import './App.css'
 
 function App() {
-  return (
-    <div className="App">
-      Hello There this is the final message hopefully
-    </div>
-  );
+    return (
+        <div className="App">
+            Hello There this is the final message hopefully
+        </div>
+    )
 }
 
-export default App;
+export default App
