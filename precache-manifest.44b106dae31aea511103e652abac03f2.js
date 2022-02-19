b31f7f53df43f42f747a29855e61458c9c72ef49
Aman-Godara <Aman-Godara@users.noreply.github.com>
2022-02-19 17:18:45 +0000

    Add API to add user details to database
    This commit acts as POC for connecting frontend with
    firebase's firestore. 20712aadb97573006da3c9d21cccd0b267ba916f

---
+++ precache-manifest.44b106dae31aea511103e652abac03f2.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b274860d169922e911251970b085b1c",
+    "url": "/crew/index.html"
+  },
+  {
+    "revision": "09cc2246730965f18384",
+    "url": "/crew/static/css/0.8cb03020.chunk.css"
+  },
+  {
+    "revision": "e36026d037be49c2c281",
+    "url": "/crew/static/css/3.e3f1d676.chunk.css"
+  },
+  {
+    "revision": "5dd133ef349b8182a5f9",
+    "url": "/crew/static/css/main.52b0f771.chunk.css"
+  },
+  {
+    "revision": "09cc2246730965f18384",
+    "url": "/crew/static/js/0.f60ee466.chunk.js"
+  },
+  {
+    "revision": "e36026d037be49c2c281",
+    "url": "/crew/static/js/3.9c0f6729.chunk.js"
+  },
+  {
+    "revision": "d09caf422e5328bff2d71c0a8609876a",
+    "url": "/crew/static/js/3.9c0f6729.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5dd133ef349b8182a5f9",
+    "url": "/crew/static/js/main.0f053afe.chunk.js"
+  },
+  {
+    "revision": "47fab3f5b9c949bcd557",
+    "url": "/crew/static/js/runtime-main.dff242f5.js"
+  }
+]);
